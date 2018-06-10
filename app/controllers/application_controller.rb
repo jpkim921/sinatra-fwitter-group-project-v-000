c21ba36732b757fada2799b3ca2e59941ac68b1b
Jayon Kim <jpkim921@gmail.com>
2018-06-10 19:14:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
     set :session_secret, "secret"
+    enable :sessions
   end
 
   helpers do
