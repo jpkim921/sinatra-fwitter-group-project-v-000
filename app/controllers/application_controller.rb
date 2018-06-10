0cc4d11a228b720c48297c43366bad82e0aea877
Jayon Kim <jpkim921@gmail.com>
2018-06-10 19:43:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,9 +22,9 @@ class ApplicationController < Sinatra::Base
 
 
     def logged_in?
-      !!sesesion[:user_id]
+      !!session[:user_id]
     end
-    
+
     def current_user
       User.find(session[:user_id])
     end
