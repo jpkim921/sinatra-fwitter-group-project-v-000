adceef9c6590b87339f26b8a94e3d26ccf7d1b7c
Jayon Kim <jpkim921@gmail.com>
2018-06-10 19:00:41 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,63 +1,51 @@
-require './config/environment'
-
-class UserController < ApplicationController
-
-  get '/' do
-    erb :'/users/homepage'
-  end
-
+class UsersController < ApplicationController
   get '/signup' do
-    # if !logged_in?
-    #   erb :'users/signup'
-    # elsif logged_in?
-    #   redirect '/tweets'
-    # end
-
     if !logged_in?
-      erb :'users/signup'
+      erb :'/users/create_user'
     else
-      redirect to '/tweets'
+      redirect '/tweets'
     end
   end
 
   post '/signup' do
-
-    # if params.values.any?{|param| param == ""}
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect '/signup'
     else
-      # @user = User.create(params)
-      # session[:id] = @user.id
-      # # binding.pry
-
-      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
-      session[:user_id] = @user.id
-
+      user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = user.id
       redirect '/tweets'
     end
   end
 
   get '/login' do
     if !logged_in?
-     erb :'users/login'
-   else
-     redirect '/tweets'
-   end
+      erb :'users/login'
+    else
+      redirect '/tweets'
+    end
   end
 
   post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/tweets"
+      redirect '/tweets'
     else
       redirect '/signup'
     end
   end
 
-
-
-
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+  
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
 end
-#
