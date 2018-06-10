445e2968f325544836ae19b860f359e6caca7cdf
Jayon Kim <jpkim921@gmail.com>
2018-06-10 18:52:35 +0000

    automatically backed up by learn

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -4,10 +4,10 @@ class TweetController < ApplicationController
 
   get '/tweets' do
     if logged_in?
-  @tweets = Tweet.all
-  erb :'tweets/tweets'
-else
-  redirect to '/login'
-end
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
   end
 end
