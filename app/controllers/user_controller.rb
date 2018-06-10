36e16a5f1819d85d304421bdc56216295e47ba39
Jayon Kim <jpkim921@gmail.com>
2018-06-10 18:45:51 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -46,7 +46,7 @@ class UserController < ApplicationController
     binding.pry
     @user = User.find_by(:username => params[:username])
     session[:user_id] = @user.id
-    
+    redirect '/tweets'
   end
 
 
