b28fc1c6e30565b7a630b4bd78ec4c55f63ac12e
Jayon Kim <jpkim921@gmail.com>
2018-06-10 18:54:51 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -57,15 +57,7 @@ class UserController < ApplicationController
   end
 
 
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
 
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
 
 end
 #
