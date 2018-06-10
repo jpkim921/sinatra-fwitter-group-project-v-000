78ea5ed2423bce4eddcf481a748c46e5867577d4
Jayon Kim <jpkim921@gmail.com>
2018-06-10 19:38:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,19 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    
+
     # need to have the two lines below to have access to the sessions hash
     enable :sessions
     set :session_secret, "secret"
   end
 
   helpers do
-    def current_user
-      @current_user ||= User.find_by_id(session[:user_id])
-    end
-
     def logged_in?
       !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id])
     end
   end
 
